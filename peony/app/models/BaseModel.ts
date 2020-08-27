73e6d3d63b31c9a6a3b81a226cf5a3ad19ea2ea2
takuya.takeo <tac829@gmail.com>
2020-08-28 02:03:10 +0900

    sample db connect.

--- peony/app/models/BaseModel.ts
+++ peony/app/models/BaseModel.ts
@@ -1,9 +1,34 @@
 import mysql from 'promise-mysql';
 
-class BaseModel {
-	private _con: object;
-	constructor() {
-		this._con = await mysql.createConnection({
+export class BaseModel
+{
+    //private _whereList: string[][] = [[],[]];
+
+	public selectAll(tableName: string): Promise<object> {
+		// Use arrow if you want to use 'this'. (NG: "function() {}", OK:"() => {}")
+		// https://stackoverflow.com/questions/52533350/angular-call-function-with-promise-in-typescript
+		return new Promise((resolve, reject) => {
+			this._connection().then(con => {
+				const res = con.query(`select * from ${tableName}`);
+				con.end();
+				resolve(res);
+			});
+		});
+	}
+
+	public selectWhere(tableName: string) {
+	}
+
+	public execQuery() {
+	}
+
+    public where(target: string, op: BaseModel.WHERE_OP, value: string) {
+        //this._whereList[] = [target, op, value];
+    }
+
+/*
+	private async _connection() {
+		return await mysql.createConnection({
 			host: 'mysql',
 			user: 'laradock',
 			password: 'secret',
@@ -11,8 +36,31 @@ class BaseModel {
 			multipleStatements: true
 		});
 	}
-
-	public Query() {
-		//todo
+*/
+	private _connection(): Promise<any> {
+		return new Promise((resolve, reject) => {
+			const con = mysql.createConnection({
+				host: 'mysql',
+				user: 'laradock',
+				password: 'secret',
+				database: 'rose',
+				multipleStatements: true
+			});
+			resolve(con);
+		});
 	}
+}
+
+// https://stackoverflow.com/questions/29844959/enum-inside-class-typescript-definition-file
+export namespace BaseModel
+{
+    export enum WHERE_OP
+    {
+        EQ = '=',
+        NE = '<>',
+        GT = '>',
+        LT = '<',
+        GE = '>=',
+        LE = '<=',
+    }
 }
--- peony/app/services/ConTestDbService.ts
+++ peony/app/services/ConTestDbService.ts
@@ -1,11 +1,14 @@
-import mysql from 'promise-mysql';
+//import mysql from 'promise-mysql';
+import { BaseModel } from '../models/BaseModel';
 
 export interface ITest {
-	res: object;
+	//res: object;
+    res: string;
 }
 
 export class ConTestDbService {
 	public async select(): Promise<ITest> {
+		// sample_01
 /*
 		mysql.createConnection({
 			host: 'mysql',
@@ -21,6 +24,8 @@ export class ConTestDbService {
 			console.log(rows);
 		});
 */
+		// sample_02
+/*
 		const conn = await mysql.createConnection({
 			host: 'mysql',
 			user: 'laradock',
@@ -30,9 +35,22 @@ export class ConTestDbService {
 		});
 		const result = await conn.query('select * from memos');
 		conn.end();
+*/
+		const m = new BaseModel();
+		// sample_03
+		m.selectAll('memos').then(function(res: object) {
+			console.log(res);
+		});
+		return {
+			res: 'empty return...'
+		}
+/*
+		const result = await m.selectAll('memos');
+		//console.log(result);
 
 		return {
 			res: result
 		}
+*/
 	}
 }
